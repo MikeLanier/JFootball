60b477cbe198ad3fa74670836323b774dbdc1186
Mike Lanier <Mike.Lanier1@comcast.net>
2017-06-11 07:30:52 -0500

    clean out teams folder

--- Teams/XLS/a.bat
+++
@@ -1,2 +0,0 @@
-call windiff "M:\Mike\Football\Teams\XLS\1979 Alabama.csv" "M:\Mike\Football\Teams\XLS\save\1979 Alabama.csv"
-call windiff "M:\Mike\Football\Teams\XLS\1979 Alabama.csv" "M:\Mike\Football\Teams\XLS\save2\1979 Alabama.csv"
