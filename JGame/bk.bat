fc4980db804b8d159da7702e7d95c9003924323c
Mike Lanier <Mike.Lanier1@comcast.net>
2017-06-11 12:29:40 -0500

    delete some unused stuff

--- JGame/bk.bat
+++
@@ -1,8 +0,0 @@
-pushd %USERPROFILE%\Dropbox\JFootball
-call backup jgame
-popd
-
-pushd %USERPROFILE%\OneDrive\JFootball
-call backup jgame
-popd
-
--- JGame/pg.bat
+++
@@ -1,8 +0,0 @@
-pushd %USERPROFILE%\Dropbox\JFootball
-call purge jgame
-popd
-
-pushd %USERPROFILE%\OneDrive\JFootball
-call purge jgame
-popd
-
